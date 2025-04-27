b412f62af7df1e5f51e10933bb02c237c22a667a
mayday <maydaythecoder@gmail.com>
2025-04-27 15:52:01 -0400

    Update app configuration for Firebase integration and improve layout structure. Change app scheme to "com.kruncheeey.pinterest" and add GoogleService-Info.plist for iOS. Include google-services.json for Android in app.json. Refactor RootLayout and HomeScreen components to enhance user experience with improved state management and a sign-out button. Clean up imports and ensure consistent formatting across files.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 import { ThemedView } from '@/components/Default/ThemedView';
 import { Feed } from '@/components/Feed';
 
-export default function HomeScreen() {
+const HomeScreen = ( { handleLogout }: { handleLogout: () => void } ) => {
 
   return (
     <ThemedView style={styles.Container}>
       <Feed />
+      <Button title="Sign Out" onPress={handleLogout} />
     </ThemedView>
   );
 }
-
+export default HomeScreen;
 const styles = StyleSheet.create({
   Container: {
     flexDirection: 'column',
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack, Redirect } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack, Redirect } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { auth } from '@/Firebase';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { auth } from "@/Firebase";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -15,7 +19,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -28,29 +32,25 @@ export default function RootLayout() {
     return null;
   }
 
-  const [user, setUser] = useState<any>(null)
+  const [user, setUser] = useState<any>(null);
 
   useEffect(() => {
     const unsubscribe = auth().onAuthStateChanged((user) => {
-      setUser(user)
-    })
-    return () => unsubscribe()
-  }, [])
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
 
   const Redirect = (user: any) => {
     if (user) {
-      return         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
+      return <Stack.Screen name="(tabs)" options={{ headerShown: false }} />;
     } else {
-      return         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-
+      return <Stack.Screen name="(auth)" options={{ headerShown: false }} />;
     }
-  }
-
-
+  };
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Redirect user={user} />
         <Stack.Screen name="+not-found" />
